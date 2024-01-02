8303845847989c123df203d5a707e15cc96ae817
Rahul-Suthar <154016388+Rahul-Suthar@users.noreply.github.com>
2024-01-02 15:04:43 +0530

    new code

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# C
+# c
+
+Hii,
+
+<br>
+
+In this repo of mine you will find the Codes of the program i have made using C language 
---
+++ new.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int main()
+{
+    int a;
+    printf("enter a :");
+
+    scanf("%d",&a);
+
+    int sum = a*a;
+
+    printf("square is : %d",sum);
+
+    return 0;
+}
