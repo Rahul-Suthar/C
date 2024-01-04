8b32871a2f60c3eb7d6fa1694e64417443c0f8fe
Rahul-Suthar <154016388+Rahul-Suthar@users.noreply.github.com>
2024-01-04 21:34:16 +0530

    two variable equation

--- Equation_solver.c
+++ Equation_solver.c
@@ -1,11 +1,23 @@
+// With the help of this code we can find the solution of linear equation having two variable ..(also two equations )
+
+
+/*Here in this code we are taking two equation as 1) aA + bB = w  and 2) xA + yB = v
+ 
+  by this  two equation we are going to find the values of two constant A & B 
+*/
+
+
 #include<stdio.h>
 #include<conio.h>
 #include<math.h>
 
+// This is function to find the values of variable and print them using main function ..
 
 float Equationsolve(int c, int d, int e, int f, int g, int h)
 {
     float A,B;
+
+// here are the general formulae to find there values ..
 
     A = (d*h - g*e) / (d*f - g*c);
     B = (c*h - f*e) / (c*g - f*d);   
@@ -18,17 +30,40 @@ float Equationsolve(int c, int d, int e, int f, int g, int h)
 
 int main()
 {
+
+// taking six constants  as there are two equations we need three-three constants per equation ..
+
+
     int a,b,w,x,y,v;
+
+// Taking  input for the constant of first equation named a ,b and w
+
 
     printf("Enter value of constant a , b and w : ");
     scanf("%d%d%d",&a,&b,&w);
 
+
+// It will gonna print the  first equation ..
+
+
     printf("Equation first is %dA + %dB = %d\n",a,b,w);
+
+
+// Taking  input for the constant of second equation named x, y and v
+
 
     printf("Enter value of constant x , y and v  : ");
     scanf("%d%d%d",&x,&y,&v);
 
+
+// It will gonna print the second equation ..
+
+
     printf("Equation second is %dA + %dB = %d\n",x,y,v);
+
+
+// Here we are recalling the above function we made to print the values of variable using main function ....
+
 
     Equationsolve(a,b,w,x,y,v);
 
